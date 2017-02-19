057fdef7014e47ad8977b19af79b931096f0d3aa
rju <rju@purdue.edu>
2017-02-18 23:42:27 -0600

    Added comments for better readability

--- evolution_res/bounding_ex.py
+++ evolution_res/bounding_ex.py
@@ -5,13 +5,17 @@ def bounds(mylist = []):
 	total = 0.0
 	maxE = 100
 
+# 	Loop through the list and make sure that all values are between 0 and 100 inclusive
 	for i in range(0,l):
 		mylist[i] = max(min(mylist[i], 100), 0)
 		total += mylist[i]
-	
+
+# 	Scale all list element values to sum to a total of 100
 	if (total != 0):
 		for i in range(0,l):
 			mylist[i] = int(mylist[i] / total * maxE)
+			
+# 	Edge Case: Equally distribute across all list elements		
 	else:
 		for i in range(0, l):
 			mylist[i] = (maxE / l)
